d557d4069122e725f06caf17362e3834ab11a125
sebastian mora villachica <moravillachicasebastian@gmail.com>
2026-03-09 18:58:48 -0600

    primer commit
    commit clase 01

---
+++ src/main/java/com/techShop/techShop/TechShopApplication.java
@@ -0,0 +1,13 @@
+package com.techShop.techShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechShopApplication.class, args);
+    }
+
+}
